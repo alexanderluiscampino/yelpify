0bbb29d68cea7b9e4c45fcdf42eb45e084fc73c7
Alexandre Campino <alexanderluiscampino@gmail.com>
2020-07-10 01:02:29 -0700

    Remove constraint - this constraint cannot be satisfied, since the name of  the business might not be unique in the business table

--- airflow/plugins/helpers/sql_queries.py
+++ airflow/plugins/helpers/sql_queries.py
@@ -10,8 +10,7 @@ class SqlQueries(Enum):
         """ALTER TABLE "business_fact" ADD FOREIGN KEY ("city_id") REFERENCES "city_fact" ("city_id");""",
         """ALTER TABLE "review_dim" ADD FOREIGN KEY ("business_id") REFERENCES "business_fact" ("business_id");""",
         """ALTER TABLE "review_dim" ADD FOREIGN KEY ("user_id") REFERENCES "users_fact" ("user_id");""",
-        """ALTER TABLE "review_fact" ADD FOREIGN KEY ("review_id") REFERENCES "review_dim" ("review_id");""",
-        """ALTER TABLE "stock_fact" ADD FOREIGN KEY ("business_name") REFERENCES "business_fact" ("name");""",
+        """ALTER TABLE "review_fact" ADD FOREIGN KEY ("review_id") REFERENCES "review_dim" ("review_id");"""
     ]
 
     drop_table_statement = "DROP TABLE IF EXISTS {TABLE_NAME} cascade;"
