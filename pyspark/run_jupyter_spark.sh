e5a0b9d529222cedaeec2666376cf9324a5844a7
Alexandre Campino <alexanderluiscampino@gmail.com>
2020-07-09 23:00:24 -0700

    Update bash script port for pyspark runner

--- pyspark/run_jupyter_spark.sh
+++ pyspark/run_jupyter_spark.sh
@@ -1,1 +1,1 @@
-docker run --rm -p 10001:8888 --name pyspark-notebook2 -v $(pwd):/home/jovyan/work jupyter/pyspark-notebook
+docker run --rm -p 8889:8888 --name pyspark-notebook2 -v $(pwd):/home/jovyan/work jupyter/pyspark-notebook
