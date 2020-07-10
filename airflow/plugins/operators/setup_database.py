53ef498858989e135dcda70a1adc42668ee32ba7
Alexandre Campino <alexanderluiscampino@gmail.com>
2020-07-10 00:59:17 -0700

    Refactor setup database oprator - Make this operator more generic and flexible, capable of running multiple statements, if a list is provided - add plenty of logging

--- airflow/plugins/operators/setup_database.py
+++ airflow/plugins/operators/setup_database.py
@@ -10,24 +10,34 @@ class SetupDatabaseOperator(BaseOperator):
     @apply_defaults
     def __init__(self,
                  redshift_conn_id="redshift",
-                 list_of_queries: dict = None,
+                 dict_of_queries: dict = None,
                  *args,
                  **kwargs):
 
         super(SetupDatabaseOperator, self).__init__(*args, **kwargs)
 
         self.redshift_conn_id = redshift_conn_id
-        self.list_of_queries = list_of_queries if list_of_queries else {}
+        self.dict_of_queries = dict_of_queries if dict_of_queries else {}
 
     def execute(self, context):
         """
         Runs a batch of queries provided in the dictionarr format query_name:query_content
 
         This operator ir normally used for set up databases, such as table and relationship creation
-        """
 
+        If multiple statements are contained in the query variable, they all are run separately
+        """
+        self.log.info(f"Running setup {self.dict_of_queries}")
         redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
-        for query_name, query in self.list_of_queries.items():
+        for query_name, query in self.dict_of_queries.items():
             self.log.info(f"Running Query: {query_name}")
-            redshift.run(query)
+            if isinstance(query, str):
+                redshift.run(query)
+                self.log.info(f"Running: {query}")
+            elif isinstance(query, list):
+                for query_entry in query:
+                    self.log.info(f"Running: {query_entry}")
+                    redshift.run(query_entry)
+            else:
+                continue
             self.log.info(f"Finished Running Query {query_name}")
